66255093de14bbcdcb162fca9627afad3ab00411
betofa85 <beto.araujo@gmail.com>
2026-03-20 10:55:14 +0000

    Adicionada tabela Fazendas

--- fastapi/models.py
+++ fastapi/models.py
@@ -2,12 +2,28 @@ from sqlalchemy import Column, Integer, String, Date, ForeignKey
 from sqlalchemy.dialects.postgresql import JSONB
 from database import Base
 
+
+# fastapi/models.py
+from sqlalchemy import Column, Integer, String, Date, Float, ForeignKey
+from sqlalchemy.dialects.postgresql import JSONB
+from database import Base
+
+class Fazenda(Base):
+    __tablename__ = "fazendas"
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String)
+    localizacao = Column(String)
+    foto_url = Column(String)
+    area_hectares = Column(Float)
+
 class Gado(Base):
     __tablename__ = "gados"
     id = Column(Integer, primary_key=True, index=True)
     brinco = Column(String)
     nome = Column(String)
-    raca_id = Column(Integer) # Simplificado para o teste inicial
     nascimento = Column(Date)
-    dados_extras = Column(JSONB) # Aqui entra seu campo de comentários/json
-    foto_url = Column(String, nullable=True)
+    foto_url = Column(String)
+    dados_extras = Column(JSONB)
+    # Adicionando a chave estrangeira
+    fazenda_id = Column(Integer, ForeignKey("fazendas.id"), default=1)
+
